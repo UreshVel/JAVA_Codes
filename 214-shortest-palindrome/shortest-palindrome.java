552aa23ee5b479b1f2dfe6b90f4cf68999c380e0
URESH VEL S <149925836+UreshVel@users.noreply.github.com>
2024-11-14 21:55:37 +0530

    Time: 502 ms (8.06%) | Memory: 45.7 MB (6.34%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+        String rev = new StringBuilder(s).reverse().toString();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.substring(0, s.length() - i).equals(rev.substring(i))) {
+                return rev.substring(0, i) + s;
+            }
+        }
+        return ""; 
+    }
+}
